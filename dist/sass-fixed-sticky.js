2f20913aa530170edeb08615ac2cacce19917363
Paul Dufour <paul@brit.co>
2014-07-08 15:35:28 -0700

    more code

--- dist/sass-fixed-sticky.js
+++ dist/sass-fixed-sticky.js
@@ -1,8 +1,10 @@
 (function() {
+  FixedSticky.tests.sticky = false;
+
   $(document).bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(e) {
     var animationName;
     animationName = e.originalEvent.animationName.trim();
-    if (animationName === 'sass-fixed-sticky-animation') {
+    if ((animationName === 'sass-fixed-sticky-animation') && !$(e.target).hasClass('fixedsticky')) {
       $(e.target).addClass('fixedsticky');
       return $(e.target).fixedsticky();
     }
